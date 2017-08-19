0c294499eee7fa15eef4a82a424a12c0f537f515
Stephanie <slrocha17@gmail.com>
2017-08-19 18:56:20 -0300

    modificações

--- resources/views/informacao/atividades.blade.php
+++ resources/views/informacao/atividades.blade.php
@@ -5,7 +5,7 @@
   <div class="jumbotron">
     <h2>Atividades Realizadas</h2> </br>
   </div>
-  <p>Abaixo segue a relação das atividades executadas por cada componente do grupo ao longo do mês de agosto:</p>                  
+  <p>Abaixo segue a relação das atividades executadas por cada componente do grupo ao longo do desenvolvimento do projeto:</p>                  
   <table class="table table-striped">
     <thead>
       <tr>
@@ -16,16 +16,30 @@
     <tbody>
       <tr>
         <td>Ana Cecília</td>
-        <td>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</td>
-
+        <td>
+	    	<ul class="circle">
+	        	<li></li>
+			</ul>
+        </td>
       </tr>
       <tr>
         <td>Stephanie Lima</td>
-        <td>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</td>
+	    <td>
+	    	<ul class="circle">
+	        	<li>Criação do Site</li>
+	        	<li>Configuração do Ambiente de Desenvolvimento</li>
+	        	<li>Configuração da base de dados</li>
+	        	<li>Participação na criação da modelagem lógica e esquema conceitual. </li>
+			</ul>
+        </td>
       </tr>
       <tr>
         <td>Thamires Asenate</td>
-        <td>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</td>
+        <td>
+	    	<ul class="circle">
+	        	<li></li>
+			</ul>
+        </td>
       </tr>
     </tbody>
   </table>
