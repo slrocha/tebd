f0e9dcc3cb6da563c4dcc7a4d28fe26a053f91fe
Stephanie <slrocha17@gmail.com>
2017-08-23 18:09:47 -0300

    criação dos controlles e atualização das rotas

--- routes/web.php
+++ routes/web.php
@@ -15,16 +15,27 @@ Route::get('/', function () {
     return view('informacao.homeTebd');
 });
 
+//rotas para pedidios
 Route::get('pedido/consultarPedido', 'PedidoController@consultarPedido')->name('pedido.consultarPedido');
-
 Route::resource('pedido', 'PedidoController');
 // Route::get('pedido/pesquisar', 'PedidoController@pesquisar');
 
 Route::resource('item', 'ItemController');
+
+//referentes a tela de informações
 Route::get('informacao/atividades', 'InformacaoController@atividades')->name('informacao.atividades');
 Route::get('informacao/documentacao', 'InformacaoController@documentacao')->name('informacao.documentacao');
 Route::get('informacao/estrategias', 'InformacaoController@estrategias')->name('informacao.estrategias');
-
-
 Route::resource('informacao', 'InformacaoController');
+
+
+Route::resource('bilhete', 'BilheteController');
+Route::resource('cliente', 'CLienteController');
+Route::resource('endereco', 'EnderecoController');
+Route::resource('espetaculo', 'EspetaculoController');
+Route::resource('poltrona', 'PoltronaController');
+Route::resource('programacao', 'ProgramacaoController');
+Route::resource('reserva', 'ReservaBilheteController');
+Route::resource('teatro', 'TeatroController');
+
 
