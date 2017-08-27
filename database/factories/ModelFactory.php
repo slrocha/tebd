7ed2724aa8b5e14eaf71722b1025aea1b14bafe0
Stephanie Lima <slrocha17@gmail.com>
2017-08-27 09:14:43 -0300

    salvando ...

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -22,3 +22,31 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
         'remember_token' => str_random(10),
     ];
 });
+
+$factory->define(App\Teatro::class, function (Faker\Generator $faker) {
+
+    return [
+        'nome' => $faker->nome,
+        'endereco' => $faker->address
+    ];
+});
+
+$factory->define(App\Endereco::class, function (Faker\Generator $faker) {
+
+    return [
+        'cidade' => $faker->city,                               
+        'bairro' => $faker->streetName,
+        'rua' => $faker->cityPrefix,
+        'numero' => $faker->buildingNumber
+    ];
+});
+
+$factory->define(App\Endereco::class, function ($faker) {
+    return [
+        'cidade' => $faker->title,
+        'bairro' => $faker->paragraph,
+        'rua' => function () {
+            return factory(App\User::class)->create()->id;
+        }
+    ];
+});
